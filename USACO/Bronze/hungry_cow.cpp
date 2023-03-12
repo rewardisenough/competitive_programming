93e02d1933b193f0f03ed0c9bb14b2f7043e9fad
rewardisenough <68491459+rewardisenough@users.noreply.github.com>
2023-03-12 14:31:30 -0500

    Update hungry_cow.cpp

--- USACO/Bronze/hungry_cow.cpp
+++ USACO/Bronze/hungry_cow.cpp
@@ -1,30 +1,4 @@
 #include <iostream>
-// using namespace std;
-
-// int main() {
-//   int n,t; cin >> n >> t;
-//   int last_d, last_b; cin >> last_d >> last_b;
-//   int total_eat = 0;
-//   int total_food = last_b;
-//   int cur_d,cur_b,dates;
-
-//   for(int i = 0; i < n-1; i++){
-//     cin >> cur_d >> cur_b;
-//     dates = cur_d-last_d;
-//     if(dates>total_food) total_eat += total_food, total_food -= total_food;
-//     else total_eat += dates, total_food -= dates;
-//     total_food += cur_b;
-//     last_d = cur_d;
-//   }
-//   // check last n to t.
-//   if(t-last_d+1 > total_food) total_eat += total_food;
-//   else total_eat += t-last_d+1;
-
-//   //output
-//   cout << total_eat << "\n";
-// }
-
-#include <iostream>
 using namespace std;
 
 int main() {
